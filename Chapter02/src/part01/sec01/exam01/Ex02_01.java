1e472917900a059eeb5b46ce09846089e5421b97
kim-dev <lunatic7208@gmail.com>
2020-07-01 17:50:48 +0900

    이클립스첫번째예제

---
+++ Chapter02/src/part01/sec01/exam01/Ex02_01.java
@@ -0,0 +1,22 @@
+package part01.sec01.exam01;
+
+public class Ex02_01 {
+
+	public static void main(String[] args) {
+		 int a,b; // 변수(저장공간)
+		 int result=0;
+		 // 주석은:프로그램 실행과 관련 없다.
+		 /* int a;
+		  * int b; int:정수
+		  */
+		 a=100;//=연산자;
+		 b=50;
+		 result=a+b;
+		 System.out.println(a+b);
+		 System.out.println(result);
+		 System.out.println("a+b="+result);
+		 System.out.println(a+"+"+b+"="+result);
+		 
+	}
+
+}
