422c0c21904d9822c0e5bcaa16b528886495996e
kim-dev <lunatic7208@gmail.com>
2020-07-02 17:47:54 +0900

    챕터4예제중간

---
+++ Chapter04/src/part01/sec01/exam01/Ex04_02.java
@@ -0,0 +1,15 @@
+
+package part01.sec01.exam01;
+
+public class Ex04_02 {
+
+	public static void main(String[] args) {
+		int a = 2, b = 3, c = 4;
+		int result1, mok, namugi;
+		double result2;
+		result1 = a + b - c;
+		System.out.printf("%d+%d-%d=%d\n", a, b, c, result1);
+		result1 = a + (b * c);
+		System.out.printf("%d+%d*%d=%d\n", a, b, c, result1);
+	}
+}
