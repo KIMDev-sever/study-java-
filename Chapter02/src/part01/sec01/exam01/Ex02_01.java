453308ff224d716ae7e262bc6e8809e8764246e2
kim-dev <lunatic7208@gmail.com>
2020-07-02 09:49:04 +0900

    2일차

--- Chapter02/src/part01/sec01/exam01/Ex02_01.java
+++ Chapter02/src/part01/sec01/exam01/Ex02_01.java
@@ -13,10 +13,15 @@ public class Ex02_01 {
 		 b=50;
 		 result=a+b;
 		 System.out.println(a+b);
-		 System.out.println(result);
+		 System.out.print(result);
 		 System.out.println("a+b="+result);
 		 System.out.println(a+"+"+b+"="+result);
-		 
+		 result=a-b;
+		 System.out.println(a+"-"+b+"="+result);
+		 result=a*b;
+		 System.out.println(a+"*"+b+"="+result);
+		 result=a/b;
+		 System.out.println(a+"/"+b+"="+result);
 	}
 
 }
---
+++ Chapter02/src/part01/sec01/exam02/ScannerExam.java
@@ -0,0 +1,12 @@
+package part01.sec01.exam02;
+
+import java.util.Scanner;
+
+public class ScannerExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner a=new Scanner(String args[]);
+	}
+
+}
