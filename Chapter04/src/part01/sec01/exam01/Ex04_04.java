eebe2383da543ae6c0af4c21340abdf63dfec36a
kim-dev <lunatic7208@gmail.com>
2020-07-03 09:51:29 +0900

    4일차시작

---
+++ Chapter04/src/part01/sec01/exam01/Ex04_04.java
@@ -0,0 +1,14 @@
+package part01.sec01.exam01;
+
+public class Ex04_04 {
+
+	public static void main(String[] args) {
+		int a=10,b;
+		b=a++;//b=10 a=11
+		System.out.printf("%d\n",b);
+		System.out.printf("%d\n",a);
+		b=++a;//b=12 a=12
+		System.out.printf("%d\n",b);
+	}
+
+}
